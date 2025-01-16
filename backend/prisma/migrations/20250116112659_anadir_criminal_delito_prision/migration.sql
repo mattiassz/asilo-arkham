98c18e36c9154fd0a70486cb8e8387924ff8cc48
mattias <nmsanchez@fi.uba.ar>
2025-01-16 08:46:13 -0300

    añadir esquema prisma

---
+++ backend/prisma/migrations/20250116112659_anadir_criminal_delito_prision/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "Criminal" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "apodo" TEXT NOT NULL,
+    "edad" INTEGER NOT NULL,
+    "genero" TEXT NOT NULL,
+    "crimen" TEXT NOT NULL,
+    "tratamientoActual" TEXT NOT NULL,
+    "celdaAsignada" INTEGER NOT NULL,
+    "fechaIngreso" TIMESTAMP(3) NOT NULL,
+    "nivelRiesgo" TEXT NOT NULL,
+    "foto" TEXT,
+
+    CONSTRAINT "Criminal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Delito" (
+    "id" SERIAL NOT NULL,
+    "tipoDelito" TEXT NOT NULL,
+    "descripcion" TEXT NOT NULL,
+    "fechaCrimen" TIMESTAMP(3) NOT NULL,
+    "sentenciaJudicial" TEXT NOT NULL,
+    "lugarDelito" TEXT NOT NULL,
+    "estadoCrimen" TEXT NOT NULL,
+    "nivelPrioridad" TEXT NOT NULL,
+
+    CONSTRAINT "Delito_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Prision" (
+    "id" SERIAL NOT NULL,
+    "tipoCelda" TEXT NOT NULL,
+    "descripcionCelda" TEXT NOT NULL,
+    "nivelSeguridad" TEXT NOT NULL,
+    "camarasSeguridad" INTEGER NOT NULL,
+    "sensoresMovimiento" BOOLEAN NOT NULL,
+    "alarmas" BOOLEAN NOT NULL,
+    "criminalAsignado" TEXT NOT NULL,
+
+    CONSTRAINT "Prision_pkey" PRIMARY KEY ("id")
+);
