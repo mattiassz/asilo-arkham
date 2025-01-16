e222f94421db2917981181d0ecd75891fbf0e34b
mattias <nmsanchez@fi.uba.ar>
2025-01-16 12:09:23 -0300

    añadir crear delito

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,36 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Arkham Asylum')
+})
+
+app.get('/api/912/delitos', async (req, res) => {
+  const delitos = await prisma.delito.findMany()
+  res.json(delitos)
+})
+
+//crear delitos
+app.post('/api/v912/delitos', async (req, res) => {
+  const delito = await prisma.delito.create({
+      data: {
+          tipoDelito: req.body.tipoDelito,
+          descripcion: req.body.descripcion,
+          fechaCrimen: new Date(req.body.fechaCrimen),
+          sentenciaJudicial: req.body.sentenciaJudicial,
+          lugarDelito: req.body.lugarDelito,
+          estadoCrimen: req.body.estadoCrimen,
+          nivelPrioridad: req.body.nivelPrioridad
+      }
+  })
+  res.status(201).send(delito)
 })
 
 app.listen(port, () => {
